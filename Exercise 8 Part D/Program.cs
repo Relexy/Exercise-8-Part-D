7e1da98e29b72ab7f2a8c8b048f15d66507daf00
Jason Tan <ac113173@avcol.school.nz>
2022-03-10 20:07:59 +1300

    Added Comments

--- Exercise 8 Part D/Program.cs
+++ Exercise 8 Part D/Program.cs
@@ -2,10 +2,10 @@
 
 int[] numbers = new int[10];
 
-
+// User Input
 int input = 0;
 
-
+// Counts Number Of Inputs
 int counter = 0;
 
 int total = 0;
@@ -15,26 +15,26 @@ while (input != -1)
 {
     if (input <= -2)
     {
-        Console.WriteLine("ERROR. \nEnter Positive Numbers Only: ");
+        Console.WriteLine("ERROR. \nEnter Positive Numbers Only: "); // If User Enters Number <= -2, ERROR And Doesn't Add To Counter
         input = Convert.ToInt32(Console.ReadLine());
     }
     else if (input > -1)
     {
         input = Convert.ToInt32(Console.ReadLine());
     }
- 
+    // User Enters Number > -1, Adds Input To Counter
     if (input > -1)
     {
         numbers[counter] = input;
         counter++;
     }
 
-    if (counter == 10)
+    if (counter == 10) // User Enters 10 Valid Numbers(x > -1), Input Automatically = -1 To Stop
     {
         input = -1;
     }
 }
-foreach (int num in numbers)
+foreach (int num in numbers) // Creats Int For Each Input In Array
 {
     total = total + num;
 }
