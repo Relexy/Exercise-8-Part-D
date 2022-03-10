f8a9fa661bd2b9222bca51a2b30bc42e0719c029
Jason Tan <ac113173@avcol.school.nz>
2022-03-10 20:04:21 +1300

    Add project files.

---
+++ Exercise 8 Part D/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Exercise 8 Part D */
+
+int[] numbers = new int[10];
+
+
+int input = 0;
+
+
+int counter = 0;
+
+int total = 0;
+
+Console.WriteLine("Enter Up To 10 Whole Numbers: ");
+while (input != -1)
+{
+    if (input <= -2)
+    {
+        Console.WriteLine("ERROR. \nEnter Positive Numbers Only: ");
+        input = Convert.ToInt32(Console.ReadLine());
+    }
+    else if (input > -1)
+    {
+        input = Convert.ToInt32(Console.ReadLine());
+    }
+ 
+    if (input > -1)
+    {
+        numbers[counter] = input;
+        counter++;
+    }
+
+    if (counter == 10)
+    {
+        input = -1;
+    }
+}
+foreach (int num in numbers)
+{
+    total = total + num;
+}
+
+Console.WriteLine($"Your Total Is: {total}");
